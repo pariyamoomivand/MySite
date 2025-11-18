f8a732026dfbdba7cd8175799c8ee282fb24e524
Pariya Moomivand <moomivandpariya4@gmail.com>
2025-11-18 21:44:09 +0330

    Routes configurations

---
+++ src/app/+pages/public-template/public-template.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-public-template',
+  imports: [RouterOutlet],
+  templateUrl: './public-template.html',
+  styleUrl: './public-template.css',
+})
+export class PublicTemplate {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,20 @@ import { AboutPage } from './+pages/about-page/about-page';
 import { ContactPage } from './+pages/contact-page/contact-page';
 import { InterestsPage } from './+pages/interests-page/interests-page';
 import { SkillsPage } from './+pages/skills-page/skills-page';
+import { PublicTemplate } from './+pages/public-template/public-template';
+import { LoginPage } from './+pages/login-page/login-page';
 
 export const routes: Routes = [
+  { path: 'public', component:PublicTemplate,children:[
   { path: 'home', component: HomePage },
   { path: 'about', component: AboutPage },
   { path: 'contact', component: ContactPage },
   { path: 'interests', component: InterestsPage },
   { path: 'skills', component: SkillsPage },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '**', redirectTo: '/home' },
+  {path:'',redirectTo:'/login',pathMatch:'prefix'},
+  {path:'**',redirectTo:'/home'}
+  ]},
+  {path:'login',component:LoginPage},
+  { path: '', redirectTo: '/public', pathMatch: 'full' },
+  { path: '**', redirectTo: '/public' },
 ];
